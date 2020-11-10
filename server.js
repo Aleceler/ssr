a6e42cd1e5e6ade071361434a5de24b5d7753f01
alexandre celer nabarrete <aleceler@gmail.com>
2020-11-10 15:59:09 -0300

    ssr with styled components

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ import { renderToString } from "react-dom/server";
 import { ServerLocation } from "@reach/router";
 import fs from "fs";
 import App from "./src/App";
+import { ServerStyleSheet, StyleSheetManager } from "styled-components";
+
+const sheet = new ServerStyleSheet();
+
+const getStyle = renderToString(sheet.collectStyles(<App />));
+
+const css = sheet.getStyleTags();
 
 const PORT = process.env.PORT || 3001;
 
 const html = fs.readFileSync("dist/index.html");
 const parts = html.toString().split("not rendered");
+
+const head = parts[0].split("<head>");
 
 const app = express();
 
@@ -20,7 +29,11 @@ app.use((req, res) => {
     </ServerLocation>
   );
 
-  res.send(`${parts[0]}${renderToString(reactMarkup)}${parts[1]}`);
+  res.send(
+    `${head[0] + "<head>"}${css}${head[1]}${renderToString(reactMarkup)}${
+      parts[1]
+    }`
+  );
   res.end();
 });
 
